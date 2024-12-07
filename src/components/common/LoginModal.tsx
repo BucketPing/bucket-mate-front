0dc49867db9bf0eec3f397badefa50d4147a33cb
dlsxody1 <dlsxody1@naver.com>
2024-12-07 18:30:15 +0900

    feat : useModal hook 완성

--- src/components/common/LoginModal.tsx
+++ src/components/common/LoginModal.tsx
@@ -1,17 +1,27 @@
+import CloseIcon from '@/assets/icons/close-icon.svg';
 import KakaoLogo from '@/assets/icons/kakao-icon.svg';
 import NaverLogo from '@/assets/icons/naver-logo.svg';
 import BucketMateLogo from '@/assets/icons/버-메.svg';
 
-const LoginModal = () => {
+const LoginModal = ({ toggleModal }: { toggleModal: () => void }) => {
   return (
     <div className='fixed inset-0 z-50 flex items-center justify-center'>
       <div className='fixed inset-0 bg-black/70' />
       <div className='relative z-50 bg-white w-[355px] h-[278px] rounded-[20px] p-5 flex flex-col'>
-        <img
-          src={BucketMateLogo}
-          alt='버킷메이트 로고'
-          className='w-[57px] h-[34px] mb-3'
-        />
+        <div className='flex justify-between mb-3'>
+          <img
+            src={BucketMateLogo}
+            alt='버킷메이트 로고'
+            className='w-[57px] h-[34px]'
+          />
+          <img
+            onClick={toggleModal}
+            src={CloseIcon}
+            alt='모달 닫기'
+            className='w-[57px] h-[34px] ml-3'
+          />
+        </div>
+
         <div className='mb-8'>
           버킷메이트와 함께 <br />
           새로운 목표를 만들어봐요 🙌
---
+++ src/hooks/common/useModal.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+export const useModal = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [portalElement, setPortalElement] = useState<Element | null>(null);
+
+  const toggleModal = () => {
+    setShowModal((prev) => !prev);
+  };
+
+  useEffect(() => {
+    setPortalElement(document.getElementById('portal'));
+  }, [showModal]);
+
+  return { showModal, setShowModal, portalElement, toggleModal };
+};
