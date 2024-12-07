4c755bac10a9b3e0b18e71567550cfc9c701759c
jypark <asa855@nate.com>
2024-12-07 23:36:30 +0900

    feat: 실시간 검색 기능 구현
    * useDebounce 훅 생성
    * mock API 연동 (json 목업데이터 활용)
    * 실시간으로 입력시 데이터 필터링 (추후 수정 예정)
    * 검색결과 분기처리

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -1,34 +1,15 @@
-const SearchResults = () => {
+import ResultItem from './ResultItem';
+import type { SearchResultsProps } from '@/types/search/search';
+
+const SearchResults = ({ bucketList }: SearchResultsProps) => {
   return (
     <div className='mx-auto w-full max-w-2xl p-4'>
-      <h2 className='text-xl font-medium text-gray-800 mb-4'>검색결과</h2>
-
-      {/* First Card */}
-      <div className='bg-[#FFFBF0] rounded-lg p-4 mb-4'>
-        <div className='flex justify-between items-center'>
-          <div>
-            <h3 className='text-lg font-medium mb-2'>아침운동 부수기!</h3>
-            <span className='inline-block bg-pink-100 text-pink-500 rounded-full px-3 py-1 text-sm'>
-              128명 참여중
-            </span>
-          </div>
-          <span className='text-2xl'>💪</span>
-        </div>
-      </div>
-
-      {/* Second Card */}
-      <div className='bg-[#F0FFF4] rounded-lg p-4'>
-        <div className='flex justify-between items-center'>
-          <div>
-            <h3 className='text-lg font-medium mb-2'>아침운동 부수기!</h3>
-            <span className='inline-block bg-green-100 text-green-600 rounded-full px-3 py-1 text-sm'>
-              128명 참여중
-            </span>
-          </div>
-          <span className='text-2xl'>💪</span>
-        </div>
-      </div>
-      <div className='w-4/5 h-px bg-gray-200 my-8 mx-auto'></div>
+      <h2 className='text-xl font-medium text-gray-800 mb-4'>
+        {bucketList.length > 0 && '검색결과'}
+      </h2>
+      {bucketList.map((v) => (
+        <ResultItem bucket={v} />
+      ))}
     </div>
   );
 };
