f9fd56d12aeae68c7f8b2f8825878688b7222b54
jypark <asa855@nate.com>
2024-12-07 11:53:56 +0900

    feat: 버킷리스트가 궁금해요 초안

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,17 @@
+import arrowRight from '@/assets/icons/arrowRight.svg';
+
 const Home = () => {
-  return <div className='bg-slate-400 h-full'>Home</div>;
+  return (
+    <div className='h-full'>
+      <div className='text-white rounded-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-4'>
+        <p className='text-xl font-bold'>당신의 버킷리스트가 궁금해요!</p>
+        <div className='flex items-center border-b-[1px] border-white w-fit'>
+          <p className='font-bold'>시작하기</p>
+          <img src={arrowRight} alt='arrow-right' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
