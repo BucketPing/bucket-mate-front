82689882fd18190657bfe9da5a2545ca47515681
dlsxody1 <dlsxody1@naver.com>
2024-12-07 18:14:07 +0900

    style : 로그인 모달 레이아웃

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,11 @@
+import LoginModal from '@/components/common/LoginModal';
+
 const Home = () => {
-  return <div className='bg-slate-400 h-full'>Home</div>;
+  return (
+    <div className='bg-slate-400 h-full'>
+      <LoginModal />
+    </div>
+  );
 };
 
 export default Home;
