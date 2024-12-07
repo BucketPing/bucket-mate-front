e1c3f21f554fce252072c6608b011a4499145639
dlsxody1 <dlsxody1@naver.com>
2024-12-07 10:12:14 +0900

    chore : react-router-dom 세팅 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,32 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    ),
+    // children: [
+    //   {
+    //     index: true,
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: 'about',
+    //     element: <About />,
+    //   },
+    // ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />,
 );
