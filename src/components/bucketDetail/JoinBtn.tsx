cd7d43608ef14653de91fd229abaf1a1a6256eee
brother1-4752 <guddlf4752@gmail.com>
2024-12-07 21:00:41 +0900

    feat: 참여버튼 클릭 시, 버킷 등록 화면으로 라우팅 기능 구현

--- src/components/bucketDetail/JoinBtn.tsx
+++ src/components/bucketDetail/JoinBtn.tsx
@@ -1,6 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+
 const JoinBtn = () => {
+  const navigate = useNavigate();
   return (
-    <div className='w-full bg-[rgba(0,115,255,1)] text-[16px] text-center font-medium text-white py-3 rounded-xl'>
+    <div
+      onClick={() => navigate('/create')}
+      className='w-full bg-[rgba(0,115,255,1)] text-[16px] text-center font-medium text-white py-3 rounded-xl'
+    >
       지금 참여하기
     </div>
   );
