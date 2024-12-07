d7c31c70620373943b870ada68ea357789ce7d51
Circlewee <pnj1118@naver.com>
2024-12-07 15:48:22 +0900

    feat: Home에 예시 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,14 @@
+import CardList from '@/components/home/card/CardList';
+import Header from '@/components/home/list/Header';
+
 const Home = () => {
-  return <div className='bg-slate-400 h-full'>Home</div>;
+  return (
+    <div className='bg-slate-400 h-full'>
+      Home
+      <Header>🔥 인기 버킷리스트</Header>
+      <CardList />
+    </div>
+  );
 };
 
 export default Home;
