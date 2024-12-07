118cba75f9bb3f275cc3bb8fe9fc9f6929b7158d
brother1-4752 <guddlf4752@gmail.com>
2024-12-07 23:40:14 +0900

    feat: 홈 화면에 로그인 모달창 팝업 기능 적용

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,27 +5,25 @@ import Banner from '@/components/home/Banner';
 import Input from '@/components/common/Input';
 import OngoingBucket from '@/components/home/OngoingBucket';
 import chevronLeft from '@/assets/icons/chevron-left.svg';
+import { useModal } from '@/hooks/common/useModal';
+import LoginModal from '@/components/common/LoginModal';
 
 const Home = () => {
   const navigate = useNavigate();
-
-  const goToLogin = () => {
-    //TODO:: 로그인 페이지 확인
-    navigate('/login');
-  };
+  const { showModal, portalElement, openModal, closeModal } = useModal();
 
   const goToSearch = () => {
     //TODO:: 검색 페이지 확인
     // navigate('/search');
   };
-  
+
   const goToDetail = () => {
     //navigate('/bucket/{id}')
   };
 
   return (
     <div className='h-full'>
-      <Banner onClick={goToLogin} />
+      <Banner onClick={() => openModal()} />
       <div className='h-full rounded-t-[20px] shadow-[0px_0px_20px_0px_#00000010] p-5'>
         <div onClick={goToSearch} className='flex cursor-pointer'>
           <Link to='/'>
@@ -60,6 +58,7 @@ const Home = () => {
           onClick={goToDetail}
         />
       </div>
+      {portalElement && showModal && <LoginModal closeModal={closeModal} />}
     </div>
   );
 };
