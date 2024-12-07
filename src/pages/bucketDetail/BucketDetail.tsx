ff7aad820495afdf670f117cb90ef9e5fe95df27
brother1-4752 <guddlf4752@gmail.com>
2024-12-07 20:51:44 +0900

    feat: 버킷상세 참여버튼 컴포넌트 구현

--- src/pages/bucketDetail/BucketDetail.tsx
+++ src/pages/bucketDetail/BucketDetail.tsx
@@ -1,18 +1,46 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import chevronLeft from '@/assets/icons/chevron-left.svg';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Thumbnail from '@/components/bucketDetail/Thumbnail';
+import Header from '@/components/bucketDetail/Header';
+import Title from '@/components/bucketDetail/Title';
+import BucketOwner from '@/components/bucketDetail/BucketOwner';
+import JoinMember from '@/components/bucketDetail/JoinMember';
+import Category from '@/components/bucketDetail/Category';
+import Description from '@/components/bucketDetail/Description';
+import Duration from '@/components/bucketDetail/duration';
+import JoinBtn from '@/components/bucketDetail/JoinBtn';
 
 const BucketDetail = () => {
+  // TODO: 추후 API 호출 시 사용
   const { id: bucketId } = useParams();
-  const navigate = useNavigate();
+
+  const [isHeartClicked, setIsHeartClicked] = useState(false);
+  const [isExpandBtnClicked, setIsExpandBtnClicked] = useState(false);
 
   return (
-    <div className='h-full'>
-      <header>
-        <button onClick={() => navigate(-1)}>
-          <img src={chevronLeft} />
-        </button>
-      </header>
-    </div>
+    <section className='m-5'>
+      <Header />
+      <Thumbnail />
+
+      <div>
+        <Title
+          isHeartClicked={isHeartClicked}
+          setIsHeartClicked={setIsHeartClicked}
+        />
+
+        <main className='mb-5'>
+          <BucketOwner />
+          <JoinMember
+            isExpandBtnClicked={isExpandBtnClicked}
+            setIsExpandBtnClicked={setIsExpandBtnClicked}
+          />
+          <Category />
+          <Description />
+          <Duration />
+          <JoinBtn />
+        </main>
+      </div>
+    </section>
   );
 };
 
