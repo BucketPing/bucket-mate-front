59af34d8a35ab261c8ba55300610fb1654404929
dlsxody1 <dlsxody1@naver.com>
2024-12-07 10:42:43 +0900

    chore : path alias 설정 완료

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vite/client" />
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+});
