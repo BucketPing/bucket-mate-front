221c2dd886c9f5fa654ecab9f72206e9220089ef
yrnoh <noh1oo1@naver.com>
2024-12-08 05:07:47 +0900

    feat: 버킷 등록 초기 작업

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import Home from '@/pages/home/Home.tsx';
 import Search from '@/pages/search/Search.tsx';
 import BucketDetail from '@/pages/bucketDetail/BucketDetail.tsx';
+import BucketCreate from './pages/bucketCreate/BucketCreate.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: '/bucket/:id',
         element: <BucketDetail />,
+      },
+      {
+        path: '/create',
+        element: <BucketCreate />,
       },
     ],
   },
