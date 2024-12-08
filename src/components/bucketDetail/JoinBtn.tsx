38609dc5f898352cb18d957e7d7b5e42469f481c
brother1-4752 <guddlf4752@gmail.com>
2024-12-09 04:21:55 +0900

    fix: 버킷상세에서 참여 버튼 클릭 시, 내 버킷리스트 화면으로 이동하는 것으로 수정

--- src/components/bucketDetail/JoinBtn.tsx
+++ src/components/bucketDetail/JoinBtn.tsx
@@ -4,8 +4,8 @@ const JoinBtn = () => {
   const navigate = useNavigate();
   return (
     <div
-      onClick={() => navigate('/create')}
-      className='w-full bg-[rgba(0,115,255,1)] text-[16px] text-center font-medium text-white py-3 rounded-xl'
+      onClick={() => navigate('/bucket-register')}
+      className='w-full bg-[rgba(0,115,255,1)] text-[16px] text-center font-medium text-white py-3 rounded-xl cursor-pointer'
     >
       지금 참여하기
     </div>
