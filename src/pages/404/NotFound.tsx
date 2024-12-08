608eb2a9f7342dc974c07f5d337da8759617b917
jypark <asa855@nate.com>
2024-12-08 22:49:05 +0900

    feat: notfound 초안

---
+++ src/pages/404/NotFound.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/common/Header';
+// import warning from '@/assets/images/warning.png';
+
+const NotFound = () => {
+  return (
+    <div className='h-screen'>
+      <Header />
+      <div className='flex flex-col items-center justify-center h-full'>
+        {/* <img src={warning} alt='warning' /> */}
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
