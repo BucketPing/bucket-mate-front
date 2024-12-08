10befb7650adfcf05549ac99efbb8b7557797d14
Circlewee <pnj1118@naver.com>
2024-12-08 16:13:18 +0900

    feat: MyPage에 연결

---
+++ src/pages/myPage/MyPage.tsx
@@ -0,0 +1,14 @@
+import Title from '@/components/common/Title';
+import MyBucketList from '@/components/myPage/MyBucketList';
+import MyInfo from '@/components/myPage/MyInfo';
+
+const MyPage = () => {
+  return (
+    <div className='h-full'>
+      <Title />
+      <MyInfo />
+    </div>
+  );
+};
+
+export default MyPage;
