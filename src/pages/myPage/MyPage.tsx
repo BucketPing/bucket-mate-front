625d0db35313f318619febc16bb6d59f28f0576e
jypark <asa855@nate.com>
2024-12-08 17:19:04 +0900

    fix: Mypage Title 컴포넌트 삭제, mock data 파일명 수정

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -1,10 +1,8 @@
-import Title from '@/components/common/Title';
 import MyInfo from '@/components/myPage/MyInfo';
 
 const MyPage = () => {
   return (
     <div className='h-full'>
-      <Title />
       <MyInfo />
     </div>
   );
